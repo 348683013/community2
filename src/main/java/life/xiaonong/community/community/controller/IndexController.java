e6b0ae51c0d6506d671763993cd9455df5887693
348683013 <348683013@qq.com>
2020-02-16 22:37:07 +0800

    添加cookie登陆

--- src/main/java/life/xiaonong/community/community/controller/IndexController.java
+++ src/main/java/life/xiaonong/community/community/controller/IndexController.java
@@ -1,15 +1,35 @@
 package life.xiaonong.community.community.controller;
 
+import life.xiaonong.community.community.mapper.UserMapper;
+import life.xiaonong.community.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by zhouzhongzhong on 2020/2/15
  */
 @Controller
 public class IndexController {
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String Hello() {
+    public String index(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if (user != null) {
+                    request.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        }
         return "index";
     }
 }
